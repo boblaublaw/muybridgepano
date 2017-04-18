0768bfcc85358a33d5903c8a25bcb76d3cb79ee5
boblaublaw <github@sublimation.org>
2017-04-17 21:04:47 -0700

    animation fill set better

--- polygon.py
+++ polygon.py
@@ -47,25 +47,20 @@ print '</a-assets>'
 for k,v in edges.iteritems():
     print '<a-plane src="#empp%02d" position="%6f 0 %6f" rotation="0 %6f 0" width="%6f" height="%6f"></a-plane>' % (k, v[0][0], v[0][1], rots[k], width, height)
 print """<a-entity camera="userHeight: 0.0; zoom: 3" look-controls>
-<a-entity position="0 0 -1"
-geometry="primitive: ring; radiusOuter: 0.015; radiusInner: 0.01;"
-material="color: cyan; shader: flat" 
-cursor="maxDistance: 30; fuse: true">
-<!--a-animation begin="click" easing="ease-in" attribute="scale"
-fill="backwards" from="0.1 0.1 0.1" to="1 1 1" dur="150"></a-animation-->
-<!--a-animation begin="fusing" easing="ease-in" attribute="scale"
-fill="forwards" from="1 1 1" to="0.1 0.1 0.1" dur="1500"></a-animation-->
-<a-text position="0 -0.25 0" align="center" color="yellow" wrap-count="300" value=""></a-text>
-<a-plane position="0 -0.255 -0.01" scale="0.3 0.04 0" color="black"></a-plane>
+<a-entity position="0 0 -1" geometry="primitive: ring; radiusOuter: 0.015; radiusInner: 0.01;" material="color: cyan; shader: flat" cursor="maxDistance: 30; fuse: true">
+  <a-text position="0 -0.25 0" align="center" color="yellow" wrap-count="300" value=""></a-text>
+  <a-plane position="0 -0.255 -0.01" scale="0.3 0.04 0" color="black"></a-plane>
 </a-entity>
 <!--a-entity position="0.3 -0.1 -1" text="color: #01ff02; value: polar coords:;"></a-entity-->
 </a-entity>
-<a-entity position="8.420 1.11 5.40" rotation="0 242.000000 0" geometry="primitive: circle; radius: 0.3" material="opacity: 0.5" color="#CCC">
-  <a-animation begin="mouseenter" attribute="position" to="4.21 .55 2.7" delay="100" dur="500" fill="both"></a-animation>
-  <a-animation begin="mouseenter" attribute="material.opacity" to="0" delay="0" dur="100" fill="both"></a-animation>
-  <a-animation begin="mouseleave" attribute="material.opacity" to="0.3" delay="1000" dur="2000" fill="both"></a-animation>
-  <a-animation begin="mouseleave" attribute="position" to="8.420 1.11 5.40" delay="1500" dur="500" fill="both"></a-animation>
-  <a-entity position="0.09 -0.37 -0.39" geometry="primitive: plane; height: 0.5; width: 1.5" color="#DDD"></a-entity>
+<a-entity position="8.420 1.11 5.40" rotation="0 242.000000 0" geometry="primitive: circle; radius: 0.3" material="opacity: 0.5" color="grey">
+  <a-animation begin="mouseenter" attribute="position" to="4.21 .55 2.7" delay="100" dur="500" fill="forward"></a-animation>
+  <a-animation begin="mouseenter" attribute="material.opacity" to="0" delay="0" dur="100" fill="forward"></a-animation>
+  <a-animation begin="mouseleave" attribute="material.opacity" to="0.3" delay="1000" dur="2000" fill="backward"></a-animation>
+  <a-animation begin="mouseleave" attribute="position" to="8.420 1.11 5.40" delay="1500" dur="500" fill="backward"></a-animation>
+  <a-entity position="0.09 -0.37 -0.39" geometry="primitive: plane; height: 0.5; width: 1.5" color="lightgrey">
+    <a-text position="0 0 0" align="center" wrap-count="100" value="This is a spite wall!" color="black"></a-entity>
+  </a-entity>
 </a-entity>
 </a-scene>
 <script>
